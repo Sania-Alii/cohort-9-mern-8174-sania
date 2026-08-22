ae9c516edc460f6eebf4a640ae440c762f2bf64e
Sania Ali <saniarajput987@gmail.com>
2026-08-22 17:41:53 +0500

    chore: complete initial frontend setup

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-700 shadow-lg p-6 bg-white rounded-2xl">
+        Frontend setup successful!
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
